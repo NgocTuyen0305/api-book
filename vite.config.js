6afde5984478491ee26ade80f7ff2038b7e45764
NgocTuyen0305 <tuyenln0305@gmail.com>
2023-11-01 15:30:57 +0700

    update vite plugin

--- vite.config.js
+++ vite.config.js
@@ -1,35 +1,23 @@
 import { defineConfig } from "vite";
 import { VitePluginNode } from "vite-plugin-node";
-import {resolve} from 'path'
 export default defineConfig(() => {
-    return {
-        root: "src",
-        server: {
-            host: '0.0.0.0',
-            port: 5000,
-        },
-        build: {
-            ssr: "index.ts",
-            outDir: "../dist",
-            emptyOutDir: true,
-            minify: true
-        },
-        plugins: [
-            ...VitePluginNode({
-                adapter: "express",
-                appPath: "./src/app.js",
-                exportName: "viteNodeApp",
-                tsCompiler: "esbuild",
-                swcOptions: {},
-            }),
-        ],
-          resolve: {
-            alias: {
-                "@": resolve(__dirname, "src"),
-            },
-        },
-        optimizeDeps: {
-            include: ['**/*.js'],
-        }
-    };
+  return {
+    root: "src",
+    server: {
+      host: "0.0.0.0",
+      port: 5000,
+    },
+    plugins: [
+      ...VitePluginNode({
+        adapter: "express",
+        appPath: "./src/app.js",
+        exportName: "viteNodeApp",
+        tsCompiler: "esbuild",
+        swcOptions: {},
+      }),
+    ],
+    optimizeDeps: {
+      include: ["express"],
+    },
+  };
 });
