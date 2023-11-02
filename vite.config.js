e5b4757bca1538df2a7d969a8b4692dab2e143e9
NgocTuyen0305 <tuyenln0305@gmail.com>
2023-11-02 09:01:06 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -25,5 +25,10 @@ export default defineConfig(() => {
     optimizeDeps: {
       include: ["express"],
     },
+    resolve: {
+      alias: {
+        "@": resolve(__dirname, "src"),
+      },
+    },
   };
 });
