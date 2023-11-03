d78ffd7420c75c2ba55457a47d503271ca3f3cd4
NgocTuyen0305 <tuyenln0305@gmail.com>
2023-11-03 15:03:56 +0700

    checkpermission slider

--- src/routes/slider.js
+++ src/routes/slider.js
@@ -1,7 +1,9 @@
 import express from "express";
-import { get,create } from '../controllers/slider.js'
+import { get, create } from "../controllers/slider.js";
+import { checkPermission } from "../middlewares/checkPimission.js";
+
 const router = express.Router();
 
-router.get("/get-slider", get);
-router.post("/create-slider", create);
+router.get("/get-slider", checkPermission, get);
+router.post("/create-slider", checkPermission, create);
 export default router;
